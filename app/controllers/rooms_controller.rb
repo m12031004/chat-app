42f72a05f2d5ad647f32880df7d056f140d6385f
m12031004 <misaki_tsuchimoto@mail.ncn-ncc.co.jp>
2026-05-15 11:29:29 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,5 @@
 class RoomsController < ApplicationController
-  def new
-    @room = Room.new
+  def index
   end
 
   def create
@@ -17,5 +16,5 @@ class RoomsController < ApplicationController
   def room_params
     params.require(:room).permit(:name, user_ids: [])
   end
-  
+
 end
